84de6110a1806916bd2f118e9704bd1652d9158b
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-26 18:13:06 -0500

    Index Changes

--- FormOperationsServlet/src/main/java/web/Servlet.java
+++ FormOperationsServlet/src/main/java/web/Servlet.java
@@ -8,27 +8,14 @@ import javax.servlet.http.*;
 
 public class Servlet extends HttpServlet{
     //Llamada a través del navegador
-    @Override
-    /*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("Hola mundo desde servlet xd");
-    }*/
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        //Leer parametros del formulario
-        String usuario = request.getParameter("usuario");
-        String password = request.getParameter("password");
-        System.out.println(usuario + password);
-        
+    @Override    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{      
         //Enviar parametroos a otra pagina html
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<body>");
-        out.println("Usuario " + usuario);
-        out.println("password " + password);
-        out.println("</body>");
-        out.println("</html>");        
+        
+        //Leer parametros del formulario
+        String operacion = request.getParameter("operacion");
+        out.print(operacion);
     }
 }
