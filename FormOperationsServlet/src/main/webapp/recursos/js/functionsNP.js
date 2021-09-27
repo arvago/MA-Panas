c752b5edc8f021d296e4ee2639eec13e871b7287
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-26 22:37:10 -0500

    Numeros Primos

--- FormOperationsServlet/src/main/webapp/recursos/js/functionsNP.js
+++ FormOperationsServlet/src/main/webapp/recursos/js/functionsNP.js
@@ -1,7 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.onload = function(){
+    document.getElementById("btnCalculate").onclick = calculateNP;
+};
+
+function calculateNP() {
+    var cantidad = document.getElementById('txtCantidad').value;
+    var txtRes = document.getElementById('txtRes');
+    var bandera = true, m, s = 2;
+    
+    for(var i=1; i<=cantidad; i++){
+        bandera = false;
+        while(!bandera){
+            m = 2;
+            bandera = true;
+            while(bandera && m < i){
+                if(s % m === 0){
+                    bandera = false;
+                }else{
+                    m = m + 1;
+                }
+            }
+            if(bandera){
+                txtRes.innerHTML = txtRes.innerText + s + ",";
+            }
+            s++;
+        }
+    }
+    
+}
 
 
