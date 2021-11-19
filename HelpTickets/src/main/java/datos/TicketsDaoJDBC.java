a42438c829f5a22bb63a4f0ed712b518060ac24f
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-11-18 23:15:16 -0600

    Some bugs fixed

--- HelpTickets/src/main/java/datos/TicketsDaoJDBC.java
+++ HelpTickets/src/main/java/datos/TicketsDaoJDBC.java
@@ -10,7 +10,7 @@ import java.sql.*;
 import java.util.*;
 
 public class TicketsDaoJDBC {
-    private static final String SQL_SELECT_TICKETS = "SELECT t.idTicket, t.topic, t.description, t.status, CONCAT(u.name, '', u.lastname) AS nombreUsuario " +
+    private static final String SQL_SELECT_TICKETS = "SELECT t.idTicket, t.topic, t.description, t.status, CONCAT(u.name, ' ', u.lastname) AS nombreUsuario " +
                                                     "FROM help_tickets.ticket t INNER JOIN help_tickets.user u ON u.idUser = t.idUserU WHERE t.idArea = ? ORDER BY t.priority ASC";
     private static final String SQL_INSERT = "INSERT INTO ticket(idArea, idUserU, status, topic, description, priority) "
             + " VALUES(?, ?, ?, ?, ?, ?)";
